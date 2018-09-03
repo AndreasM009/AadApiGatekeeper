d29b9da9444cdcbe531f4f57c4a35ec5268a56e3
Andreas Mock <andreas.mock@live.com>
2018-09-03 16:14:58 +0200

    Enabled developer exception page for temp bugfixing

--- src/AadApiGatekeeper/Startup.cs
+++ src/AadApiGatekeeper/Startup.cs
@@ -29,10 +29,10 @@ namespace AadApiGatekeeper
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         { 
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             app.UseAuthProxy();                     
             app.UseMvc();
